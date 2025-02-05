44f95c4321d7f698239e7acd21285bde6580eb2e
Nors <sron.niko@gmail.com>
2025-02-05 14:12:40 +0100

    teste

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -3,21 +3,21 @@ import { useState } from "react";
 const CounterPage = () => {
     /**
      * TODO Déclaration de l'état stockant la valeur du compteur
-     * 
+     *
      * Plus d'information sur la déclaration des useState : https://www.carlrippon.com/typed-usestate-with-typescript/
      */
 
 
     /**
      * TODO implémenter Fonction permettant d'incrémenter l'état du compteur.
-     * 
+     *
      * Utilisée en tant que "handler" d'un évènement "click".
      */
 
 
     /**
      * TODO implémenter un fonction permettant d'incrémenter l'état du compteur
-     * 
+     *
      * Utilisée en tant que "handler" d'un évènement "click".
      */
 
